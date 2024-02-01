5a685dc6b1615cd6d06b3b3711e5d995f8617b21
patrick <pa.trickjlp@gmail.com>
2024-02-01 14:01:38 +0100

    add tests

---
+++ tests/babel-plugin.test.js
@@ -0,0 +1,11 @@
+import babel from '@babel/core';
+
+import { describe, expect, it } from 'vitest';
+
+import plugin from '../dist/lib/babel-plugin';
+
+describe('convert template with hot reload helpers', () => {
+  it('should convert correctly', () => {
+
+  });
+});
